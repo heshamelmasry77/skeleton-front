0b359fda25c2a0bb437eaf779fb7384d09abd4c3
heshamelmasry77 <heshamelmasry@outlook.com>
2017-08-20 14:53:51 +0200

    Skeleton commit one

---
+++ src/app/pages/dashboard/controllers/DashboardController.js
@@ -0,0 +1,23 @@
+/**
+ * @author heshamelmasry
+ * created on 23.03.2017
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.dashboard')
+        .controller('DashboardController', DashboardController);
+
+    /** @ngInject */
+    function DashboardController() {
+
+        var vm = this;
+        vm.init = init;
+
+        function init() {
+
+
+        }
+    }
+})();
